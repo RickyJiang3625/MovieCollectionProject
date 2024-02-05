2afbd10db57700ef9cf371150316039c83b471da
BT_4N2_23 <asdas@net.casfd>
2024-02-05 14:02:56 -0500

    searchKeyword method and working on searchCast

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MovieCollection
@@ -192,6 +193,65 @@ public class MovieCollection
         // now, display them all to the user
         for (int i = 0; i < results.size(); i++)
         {
+            String cast = results.get(i).getCast();
+            String[] castTemp=new String[cast.split("\\|").length];
+            for(int j=0;j<castTemp.length;j++){
+                if(castTemp[j].contains(searchTerm)){
+                    cast=castTemp[j];
+                }
+            }
+
+
+            // this will print index 0 as choice 1 in the results list; better for user!
+            int choiceNum = i + 1;
+
+            System.out.println("" + choiceNum + ". " + cast);
+        }
+
+        System.out.println("Which movie would you like to learn more about?");
+        System.out.print("Enter number: ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+
+        Movie selectedMovie = results.get(choice - 1);
+
+        displayMovieInfo(selectedMovie);
+
+        System.out.println("\n ** Press Enter to Return to Main Menu **");
+        scanner.nextLine();
+    }
+
+    private void searchKeywords()
+    {
+        System.out.print("Enter a keyword search term: ");
+        String searchTerm = scanner.nextLine();
+
+        // prevent case sensitivity
+        searchTerm = searchTerm.toLowerCase();
+
+        // arraylist to hold search results
+        ArrayList<Movie> results = new ArrayList<Movie>();
+
+        // search through ALL movies in collection
+        for (int i = 0; i < movies.size(); i++)
+        {
+            String keywords = movies.get(i).getKeywords();
+            keywords = keywords.toLowerCase();
+
+            if (keywords.indexOf(searchTerm) != -1)
+            {
+                //add the Movie objest to the results list
+                results.add(movies.get(i));
+            }
+        }
+
+        // sort the results by title
+        sortResults(results);
+
+        // now, display them all to the user
+        for (int i = 0; i < results.size(); i++)
+        {
             String title = results.get(i).getTitle();
 
             // this will print index 0 as choice 1 in the results list; better for user!
@@ -212,11 +272,6 @@ public class MovieCollection
 
         System.out.println("\n ** Press Enter to Return to Main Menu **");
         scanner.nextLine();
-    }
-
-    private void searchKeywords()
-    {
-
     }
 
     private void listGenres()
